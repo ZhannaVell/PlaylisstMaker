19ae654b04b7de3f503547f3aa340236cb0d7668
zhanna <6725475@gmail.com>
2026-08-04 15:19:14 +0300

    исправлен класс Track

--- app/src/main/java/com/example/playlisstmaker/Track.kt
+++ app/src/main/java/com/example/playlisstmaker/Track.kt
@@ -1,11 +1,13 @@
 package com.example.playlisstmaker
 
 
-import android.os.Parcel
+
 
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 
+@Parcelize
 data class Track(
     val trackId: Long,
     val trackName: String,
@@ -17,41 +19,10 @@ data class Track(
     val primaryGenreName: String?,
     val country: String?
 ) : Parcelable{
+
     fun getCoverArtwork(): String {
         return artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
     }
-    override fun describeContents(): Int = 0
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeLong(trackId)
-        parcel.writeString(trackName)
-        parcel.writeString(artistName)
-        parcel.writeString(trackTime)
-        parcel.writeString(artworkUrl100)
-        parcel.writeString(collectionName)
-        parcel.writeString(releaseDate)
-        parcel.writeString(primaryGenreName)
-        parcel.writeString(country)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Track> {
-        override fun createFromParcel(parcel: Parcel): Track {
-            return Track(
-                parcel.readLong(),
-                parcel.readString() ?: "",
-                parcel.readString() ?: "",
-                parcel.readString() ?: "",
-                parcel.readString() ?: "",
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString()
-            )
-        }
-
-        override fun newArray(size: Int): Array<Track?> {
-            return arrayOfNulls(size)
-        }
-    }
 }
 
